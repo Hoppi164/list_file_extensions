5d1abb6ce355184d8a7c0472bb4583d1c20d0e3f
Andrew Hopkins <Hoppi164@gmail.com>
2022-04-26 21:20:06 +1000

    Added ability to reverse the final sort order

--- lfe.sh
+++ lfe.sh
@@ -1,1 +1,24 @@
-find . -type f | tr '[:upper:]' '[:lower:]' | grep -E ".*\.[a-zA-Z0-9]*$" | sed -e 's/.*\(\.[a-zA-Z0-9]*\)$/\1/' | sort | uniq -c | sort -n
+reverse=false
+while getopts 'r' OPTION; do
+    case $OPTION in
+        r)
+            reverse=true
+            ;;
+        *)
+            echo "Usage: lfe.sh [-r]"
+            exit 1
+            ;;
+    esac
+done
+
+find . -type f | 
+tr '[:upper:]' '[:lower:]' | 
+grep -E ".*\.[a-zA-Z0-9]*$" | 
+sed -e 's/.*\(\.[a-zA-Z0-9]*\)$/\1/' | 
+sort | 
+uniq -c | 
+if "$reverse" = true ; then
+    sort -nr
+else
+    sort -n
+fi
