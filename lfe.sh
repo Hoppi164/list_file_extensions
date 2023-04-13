60000f2f867ecd9db2e28c9e7702bc4684204e86
Hoppi164 <Hoppi164@gmail.com>
2023-04-13 12:13:26 +1000

    Reverted code before introduction of -d flag

--- lfe.sh
+++ lfe.sh
@@ -1,1 +1,27 @@
-find . -type f | tr '[:upper:]' '[:lower:]' | grep -E ".*\.[a-zA-Z0-9]*$" | sed -e 's/.*\(\.[a-zA-Z0-9]*\)$/\1/' | sort | uniq -c | sort -n
+is_ascending=true
+while getopts 'ad' OPTION; do
+    case $OPTION in
+        a)
+            is_ascending=true
+            ;;
+        d)
+            is_ascending=false
+            ;;
+        *)
+            echo "Usage: lfe.sh [-a|-d]"
+            exit 1
+            ;;
+    esac
+done
+
+find . -type f | 
+tr '[:upper:]' '[:lower:]' | 
+grep -E ".*\.[a-zA-Z0-9]*$" | 
+sed -e 's/.*\(\.[a-zA-Z0-9]*\)$/\1/' | 
+sort | 
+uniq -c | 
+if "$is_ascending" = true ; then
+    sort -n
+else
+    sort -nr
+fi
